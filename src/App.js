703fe69338cda4d8223e3357ee550267b1f0a94b
DAVIDREACTS <91912539+DAVIDREACTS@users.noreply.github.com>
2021-11-22 16:52:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Header from './components/ui/header.js';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState(''); //passing query as prop from search to app.js, then setting it
+
+  //takes arrow function - use async await
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      //console.log(result.data);
+      setItems(result.data); //adjusts state so it has all that data
+      setIsLoading(false); //once characters are loaded, no need to load anymore
+    }
+    fetchItems()
+  }, [query]) //setting query as dependency - whenever query changes, useEffect function activates
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
 
 export default App;
+
